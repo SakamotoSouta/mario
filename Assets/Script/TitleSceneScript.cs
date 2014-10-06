394d855f5e096098e8c6acf86cd5c08257b31829
Souta Sakamoto <tkintern13@gmail.com>
2014-10-06 18:01:57 +0900

    タイトルからゲームへ画面遷移の実装
    タイトルからエンターキーでゲームシーンへ遷移する処理を追加

---
+++ Assets/Script/TitleSceneScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleSceneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// エンターで画面遷移
+		if(Input.GetKey(KeyCode.Return)){
+			Application.LoadLevel("Game");
+		}
+	}
+
+	// 仮でGUIで出力
+	void OnGUI(){
+		GUI.Label (new Rect(Screen.width / 2 - 100, Screen.height / 2, Screen.width / 2 + 100, Screen.height / 2 + 100), "Press START");
+	}
+}
