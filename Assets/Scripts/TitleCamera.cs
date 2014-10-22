ffb038e44073504c2f538c6f060df24215078d05
Souta Sakamoto <tkintern13@gmail.com>
2014-10-22 19:03:02 +0900

    タイトルシーンいじりはじめ
    デモとうの下準備をしているところ

---
+++ Assets/Scripts/TitleCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleCamera : MonoBehaviour {
+	public GameObject Target;
+	public Vector3 CameraOffet;
+	private float CameraCount;
+	// Use this for initialization
+	void Start () {
+		if (!Target) {
+			Target = gameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CameraCount += 0.01f;
+		if (Target) {
+			transform.Translate(Mathf.Cos(CameraCount % (Mathf.PI * 2)), 0, Mathf.Sin(CameraCount % (Mathf.PI * 2)));
+			transform.LookAt (Target.transform);
+		}
+
+	}
+}
