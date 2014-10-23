b7dde3a2bf230219c9da316e6f8ffa0e1ccc2535
Souta Sakamoto <tkintern13@gmail.com>
2014-10-23 11:14:49 +0900

    タイトルで放置するとでもに行く処理の実装
    Unity ちゃん自体が動かなくてもいいということなのでカメラがある程度フィールドを見て帰ってくるだけ

--- Assets/Scripts/TitleCamera.cs
+++ Assets/Scripts/TitleCamera.cs
@@ -2,23 +2,44 @@
 using System.Collections;
 
 public class TitleCamera : MonoBehaviour {
-	public GameObject Target;
-	public Vector3 CameraOffet;
-	private float CameraCount;
+	private float CameraCount = 0;
+	public Vector3 CameraDefaultPosition;
+	private int TitleCounter;
+	private bool Demo = false;
+	private Vector3 oldMousePosition;
+
 	// Use this for initialization
 	void Start () {
-		if (!Target) {
-			Target = gameObject;
-		}
+
+		transform.position = CameraDefaultPosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		CameraCount += 0.01f;
-		if (Target) {
-			transform.Translate(Mathf.Cos(CameraCount % (Mathf.PI * 2)), 0, Mathf.Sin(CameraCount % (Mathf.PI * 2)));
-			transform.LookAt (Target.transform);
+		TitleCounter++;
+
+		if(TitleCounter > 300){
+			Demo = true;
 		}
 
+		if (Input.anyKey || Input.mousePosition != oldMousePosition) {
+			TitleCounter = 0;
+			CameraCount = 0;
+			Demo = false;
+			transform.position = CameraDefaultPosition;
+		}
+
+		if(Demo){
+			CameraCount += 0.001f;
+			if(CameraCount > Mathf.PI){
+				Demo = false;
+				CameraCount = 0;
+				TitleCounter = 0;
+				transform.position = CameraDefaultPosition;
+			}
+			transform.position = new Vector3(CameraDefaultPosition.x + Mathf.Sin(CameraCount) * 100, transform.position.y, transform.position.z);
+
+		}
+		oldMousePosition = Input.mousePosition;
 	}
 }
